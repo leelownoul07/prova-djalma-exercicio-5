07f8baff12e6e54765900812f78b1ba1634f0ddc
jaime <105826772+leelownoul07@users.noreply.github.com>
2022-07-06 16:53:21 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace prova_djalma_exercicio_5
         {
             Console.WriteLine("numeros pares de 1 a 100");
             for (int i = 0; i<=100; i+=2)
-                Console.WriteLine($"number: {i}");
+                Console.WriteLine($"numeros: {i}");
             Console.ReadKey();
         }
     }
