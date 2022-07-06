08274a945f446bb60fe3698ddd3f0b3bd79d0bc4
Jaime <Jaime@DESKTOP-BVSIG8L>
2022-07-06 14:21:04 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prova_djalma_exercicio_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("numeros pares de 1 a 100");
+            for (int i = 0; i<=100; i+=2)
+                Console.WriteLine($"number: {i}");
+            Console.ReadKey();
+        }
+    }
+}
